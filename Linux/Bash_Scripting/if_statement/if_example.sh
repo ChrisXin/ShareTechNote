e85994b7a99cc67946c157f91ba8d238a3181431
Chris Xin Zou <Chris@Chriss-MacBook-Air.local>
2015-08-13 19:43:00 -0400

    linux

---
+++ Linux/Bash_Scripting/if_statement/if_example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Basic if statement
+
+data=500
+
+if [ $data -gt 100 ] 
+then
+  echo "Hey that's a large number."
+  pwd
+fi
+
+date
+
---
+++ Linux/Bash_Scripting/loop/for_loop_series.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Basic range in for loop
+
+for value in {1..5}
+do
+    echo $value
+done
+
+echo first part done
+
+
+for value in {10..0..2}
+do
+    echo $value
+done
+
+echo All done
+
+
